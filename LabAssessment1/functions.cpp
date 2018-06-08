fffd460906bfc26f23d82e9c735ac214ae8c8b26
Sidharth Singh <sidharth2266@outlook.com>
2018-06-08 16:05:41 +0530

    completed function definitions.

---
+++ LabAssessment1/functions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void PassByValue(int num) {
+	cout << "Inside PassByValue()" << endl;
+	num += 50;
+	cout << "num = " << num << endl;
+}
+
+void PassByRef(int &num) {
+	cout << "Inside PassByRef()" << endl;
+	num *= 5;
+	cout << "num = " << num;
+}
