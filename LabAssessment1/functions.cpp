1bab2514045ca838f5903d2fa7e059cf8b729d44
Sidharth Singh <sidharth2266@outlook.com>
2018-06-08 17:19:47 +0530

    fixed printing errors

--- LabAssessment1/functions.cpp
+++ LabAssessment1/functions.cpp
@@ -9,6 +9,7 @@ void PassByValue(int num) {
 
 void PassByRef(int &num) {
 	std::cout << "Inside PassByRef()" << std::endl;
+	std::cout << "received value is : " << num << std::endl;
 	num = 50;
 	std::cout << "num = " << num<<std::endl;
 }
