9698ece2aea4d56869c74a19f90ca554f1e3d97f
Sidharth Singh <sidharth2266@outlook.com>
2018-06-08 15:13:05 +0530

    created function definitions

--- LabAssessment1/functions.cpp
+++ LabAssessment1/functions.cpp
@@ -1,4 +1,11 @@
-int PassByRef(int &)
+#include <iostream>
+void PassByRef(int &num)
 {
-	return 0;
+	std::cout << "Inside PassByRef()" << std::endl;
+}
+int PassByValue(int num) {
+	std::cout << "Inside PassByValue()" << std::endl;
+	num *= 5;
+	std::cout << "The modified value is :" << num;
+	return num;
 }
