6d0aef79ba6cfd8c0987d0541f29da78bdd1ee59
Sidharth Singh <sidharth2266@outlook.com>
2018-06-08 17:27:39 +0530

    extra output for readablitiy.

--- LabAssessment1/functions.cpp
+++ LabAssessment1/functions.cpp
@@ -2,14 +2,15 @@
 #include <iostream>
 
 void PassByValue(int num) {
-	std::cout << "Inside PassByValue()" << std::endl;
+	std::cout << std::endl<<"INSIDE PassByValue() :" << std::endl;
+	std::cout << "Received value is : " << num << std::endl;
 	num += 50;
 	std::cout << "num = " << num << std::endl;
 }
 
 void PassByRef(int &num) {
-	std::cout << "Inside PassByRef()" << std::endl;
-	std::cout << "received value is : " << num << std::endl;
+	std::cout << std::endl << "INSIDE PassByRef() :" << std::endl;
+	std::cout << "Received value is : " << num << std::endl;
 	num = 50;
 	std::cout << "num = " << num<<std::endl;
 }
