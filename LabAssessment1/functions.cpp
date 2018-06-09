45eaa7265c1472b99832e7f3a16b66dbb0d1d611
Sidharth Singh <sidharth2266@outlook.com>
2018-06-09 16:01:13 +0530

    Completed lab assessment.

--- LabAssessment1/functions.cpp
+++ LabAssessment1/functions.cpp
@@ -12,5 +12,5 @@ void PassByRef(int &num) {
 	std::cout << std::endl << "INSIDE PassByRef() :" << std::endl;
 	std::cout << "Received value is : " << num << std::endl;
 	num = 50;
-	std::cout << "num = " << num<<std::endl;
+	std::cout << "num = " << num << std::endl;
 }
